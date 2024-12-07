514f7ab5496eddb559be759d8cacd05c8252096e
20233tn193 <20233tn193@utez.edu.mx>
2024-12-06 23:21:05 -0600

    Primer Commit

---
+++ Api-Supermercado/src/main/java/com/company/casilleros/backend/apisupermercado/ApiSupermercadoApplication.java
@@ -0,0 +1,13 @@
+package com.company.casilleros.backend.apisupermercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSupermercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSupermercadoApplication.class, args);
+	}
+
+}
---
+++ Api-Supermercado/src/test/java/com/company/casilleros/backend/apisupermercado/ApiSupermercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.casilleros.backend.apisupermercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSupermercadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
